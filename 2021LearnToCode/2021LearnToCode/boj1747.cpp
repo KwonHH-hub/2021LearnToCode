7f1ef849481e0b2e507e4c5ebbc6d2adb509c0ee
KwonHH-hub <zkzh6hn9@naver.com>
2021-01-25 21:19:37 +0900

    boj1747

---
+++ 2021LearnToCode/2021LearnToCode/boj1747.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+bool palindrome(int n) {
+	string temp1 = to_string(n);
+	string temp2 = "";
+
+	for (int i = temp1.size() - 1; i >= 0; i--)
+		temp2 += temp1[i];
+
+	if (temp1 == temp2) return true;
+	else return false;
+}
+
+bool check_prime(int n) {
+	if (n == 1) return false;
+	if (n == 2) return true;
+	else if (n % 2 == 0) return false;
+
+	int ref = sqrt(n);
+	for (int i = 3; i < ref + 1; i += 2) {
+		if (n == 3) return true;
+		else if (n % 3 == 0) return false;
+		if (n % i == 0) return false;
+	}
+	return true;
+}
+
+int main() {
+	int num = 0;
+	cin >> num;
+
+
+	while (1) {
+		if (palindrome(num) == true && check_prime(num) == true) {
+			cout << num;
+			break;
+		}
+		num++;
+	}
+	return 0;
+}
